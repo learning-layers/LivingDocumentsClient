c75be6f35934f30275798c49cd2d871106d1b76b
Martin Bachl <martin@bachl.pro>
2014-06-11 00:52:28 +0200

    Changed karma test to use chrome.

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -19,7 +19,7 @@ module.exports = function ( karma ) {
       'src/assets/**/*.js'
     ],
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-coffee-preprocessor' ],
+    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-chrome-launcher', 'karma-phantomjs-launcher', 'karma-coffee-preprocessor' ],
     preprocessors: {
       '**/*.coffee': 'coffee',
     },
@@ -54,7 +54,7 @@ module.exports = function ( karma ) {
      * the aesthetic advantage of not launching a browser every time you save.
      */
     browsers: [
-      'Firefox'
+      'Chrome'
     ],
     //hostname: process.env.IP,
     //port: process.env.PORT,
