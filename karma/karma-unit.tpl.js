fcb2862dc87a128a688143cc76c86ac69e708685
Martin Bachl <martin@bachl.pro>
2014-10-24 13:22:34 +0200

    Added phantomjs dependency.

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -19,7 +19,7 @@ module.exports = function ( karma ) {
       'src/assets/**/*.js'
     ],
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-chrome-launcher', 'karma-coffee-preprocessor' ],
+    plugins: [ 'karma-jasmine', 'karma-chrome-launcher', 'karma-phantomjs-launcher', 'karma-coffee-preprocessor' ],
     preprocessors: {
       '**/*.coffee': 'coffee',
     },
